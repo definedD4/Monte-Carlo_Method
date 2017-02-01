bab26541e43200747365d55ff926ad8e39e6b1a3
Dima <Dima@DESKTOP-LN5K810>
2017-02-01 23:05:13 +0200

    Add data normalization to view table dialog

--- Monte-Carlo_Method_3D/Monte-Carlo_Method_3D/Dialogs/ShowTableDialog.xaml.cs
+++ Monte-Carlo_Method_3D/Monte-Carlo_Method_3D/Dialogs/ShowTableDialog.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -16,6 +15,7 @@ using Monte_Carlo_Method_3D.Util;
 using Monte_Carlo_Method_3D.Visualization;
 using Monte_Carlo_Method_3D.VisualizationModel;
 using Monte_Carlo_Method_3D.VisualizationProvider;
+using static System.Math;
 
 namespace Monte_Carlo_Method_3D.Dialogs
 {
@@ -30,11 +30,14 @@ namespace Monte_Carlo_Method_3D.Dialogs
         }
 
         private readonly GridData m_Data;
+        private readonly GridData m_NormalizedData;
 
-        // TODO: Fix bugs with rendering
+        public SelectorCommand VisualTypeSelector { get; }
+
         public ShowTableDialog(GridData data)
         {
             m_Data = data;
+            m_NormalizedData = NormalizeData(data);
 
             InitializeComponent();
 
@@ -52,11 +55,11 @@ namespace Monte_Carlo_Method_3D.Dialogs
                         break;
                     case "2D":
                         VisualizationPresenter.Visualization =
-                            GridDataVisualizationProvider.Color().ProvideVisualization(m_Data);
+                            GridDataVisualizationProvider.Color().ProvideVisualization(m_NormalizedData);
                         break;
                     case "3D":
                         VisualizationPresenter.Visualization =
-                            GridDataVisualizationProvider.Model3D(m_Data.Size).ProvideVisualization(m_Data);
+                            GridDataVisualizationProvider.Model3D(m_Data.Size).ProvideVisualization(m_NormalizedData);
                         break;
                 }
             };
@@ -64,33 +67,28 @@ namespace Monte_Carlo_Method_3D.Dialogs
             TitleTb.Text = $"Таблица: {m_Data.Size.Height} на {m_Data.Size.Width}";
 
             VisualTypeSelector.RaiseSelectionChanged();
-            
-            /*Img.Source = DrawingUtil.DrawTable(data, BackgroundColor, ForegroundColor, GridPen);
-
-            Img.MouseMove += (s, e) =>
-            {
-                var pos = e.GetPosition(Img);
-
-                int x = (int)Math.Truncate(pos.X * data.Size.Width / Img.ActualWidth);
-                int y = (int)Math.Truncate(pos.Y * data.Size.Height / Img.ActualHeight);
-
-                if(x < 0 || x >= m_Data.Size.Width || y < 0 || y >= m_Data.Size.Height)
-                    return;
-
-                if (!PointedValuePopup.IsOpen) { PointedValuePopup.IsOpen = true; }
-
-                PointedValuePopup.HorizontalOffset = pos.X + 20;
-                PointedValuePopup.VerticalOffset = pos.Y;
-
-                PointedValueText.Text = m_Data[x,y].ToString("E4");
-            };
-
-            Img.MouseLeave += (s, e) =>
-            {
-                PointedValuePopup.IsOpen = false;
-            };*/
         }
 
-        public SelectorCommand VisualTypeSelector { get; }
+        private static GridData NormalizeData(GridData data)
+        {
+            double min = double.MaxValue, max = double.MinValue;
+
+            foreach (var idx in data.Bounds.EnumerateRegion())
+            {
+                min = Min(min, data[idx]);
+                max = Max(max, data[idx]);
+            }
+
+            double magnitude = max - min;
+
+            var newData = GridData.AllocateLike(data);
+
+            foreach (var idx in data.Bounds.EnumerateRegion())
+            {
+                newData[idx] = (data[idx] - min) / magnitude;
+            }
+
+            return newData;
+        }
     }
 }
